f80f1b04cf1e66edc62e1472d1fd83f8dfc9f59f
Max Hill <max@hilloco.se>
2016-07-16 18:39:39 +0200

    Better welcoming

--- template/src/views/Home.vue
+++ template/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        <h1>Welcome home!<h1>
+    <div class="container">
+        <h1 class="Hero__title">Welcome home!</h1>
+        <p class="Hero__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, ab explicabo labore facere saepe iure accusantium molestias veniam cupiditate sit reprehenderit assumenda dignissimos. Placeat sapiente natus vero, velit quia eum.</p>
     </div>
 </template>
 
